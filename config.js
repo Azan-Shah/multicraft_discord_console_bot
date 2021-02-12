8c10a43d5f98f9b99b681019d125a6a572b4bc21
Azan-Shah <69967451+Azan-Shah@users.noreply.github.com>
2021-02-12 22:23:05 +0800

    .

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
 	/* The token of your Discord Bot */
-	token: "Nzk4MTcwMTU0NDE2MzQxMDEy.X_xH4g.Zhg-gYFbPkxAyb9zl4RN36KKIaM",
+	token: "",
 	prefix: '\\', // The default prefix for the bot
 	/* Bot's owner informations */
 	owner: {
-		id: "332284443190362132", // The ID of the bot's owner
-		name: "BlackGrim#0433" // And the name of the bot's owner
+		id: "", // The ID of the bot's owner
+		name: "" // And the name of the bot's owner
 	},
 	// Multicraft API Info
-	url: "https://panel.pebblehost.com/api.php",
-	user: "nor.azan97@gmail.com",
-	key: "+wJALE$x-eThi+",
+	url: "",
+	user: "",
+	key: "",
 	serverid: "150890",
 	/* The Bot status */
 	status: {
